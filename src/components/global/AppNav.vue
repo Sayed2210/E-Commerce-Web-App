9469ec0aa1651ca68eeb180fe3dbeed68447f535
Elsayed Hassan <eh600039@gmail.com>
2023-08-06 15:31:40 +0300

    finish nav bar

--- src/components/global/AppNav.vue
+++ src/components/global/AppNav.vue
@@ -6,11 +6,31 @@
           <img src="@/assets/images/logo.png" alt="" class="mt-2" />
         </v-col>
         <v-col cols="5">
-          <input
-            type="search"
-            placeholder="Search The Store"
-            class="bg-white w-75 pa-2 mt-2"
-          />
+          <div class="search postion-relative w-100" style="position: relative">
+            <input
+              type="search"
+              placeholder="Search The Store"
+              class="bg-white w-100 pa-2 mt-2"
+            />
+            <svg
+              data-icon="search"
+              viewBox="0 0 512 512"
+              width="100%"
+              height="100%"
+              style="
+                position: absolute;
+                top: 54%;
+                transform: translateY(-50%);
+                right: 10px;
+                fill: #2196f3;
+                width: 25px;
+              "
+            >
+              <path
+                d="M495,466.2L377.2,348.4c29.2-35.6,46.8-81.2,46.8-130.9C424,103.5,331.5,11,217.5,11C103.4,11,11,103.5,11,217.5   S103.4,424,217.5,424c49.7,0,95.2-17.5,130.8-46.7L466.1,495c8,8,20.9,8,28.9,0C503,487.1,503,474.1,495,466.2z M217.5,382.9   C126.2,382.9,52,308.7,52,217.5S126.2,52,217.5,52C308.7,52,383,126.3,383,217.5S308.7,382.9,217.5,382.9z"
+              ></path>
+            </svg>
+          </div>
         </v-col>
         <v-col :cols="4" class="d-flex align-center justify-space-between">
           <div class="d-flex flex-column align-center">
@@ -18,20 +38,22 @@
             <h2 class="text-caption">+20 01013453391</h2>
           </div>
           <div class="icons text-center" @click="fireemit">
-            <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-              <path
-                class="path1"
-                d="M409.6 1024c-56.464 0-102.4-45.936-102.4-102.4s45.936-102.4 102.4-102.4S512 865.136 512 921.6 466.064 1024 409.6 1024zm0-153.6c-28.232 0-51.2 22.968-51.2 51.2s22.968 51.2 51.2 51.2 51.2-22.968 51.2-51.2-22.968-51.2-51.2-51.2z"
-              ></path>
-              <path
-                class="path2"
-                d="M768 1024c-56.464 0-102.4-45.936-102.4-102.4S711.536 819.2 768 819.2s102.4 45.936 102.4 102.4S824.464 1024 768 1024zm0-153.6c-28.232 0-51.2 22.968-51.2 51.2s22.968 51.2 51.2 51.2 51.2-22.968 51.2-51.2-22.968-51.2-51.2-51.2z"
-              ></path>
-              <path
-                class="path3"
-                d="M898.021 228.688C885.162 213.507 865.763 204.8 844.8 204.8H217.954l-5.085-30.506C206.149 133.979 168.871 102.4 128 102.4H76.8c-14.138 0-25.6 11.462-25.6 25.6s11.462 25.6 25.6 25.6H128c15.722 0 31.781 13.603 34.366 29.112l85.566 513.395C254.65 736.421 291.929 768 332.799 768h512c14.139 0 25.6-11.461 25.6-25.6s-11.461-25.6-25.6-25.6h-512c-15.722 0-31.781-13.603-34.366-29.11l-12.63-75.784 510.206-44.366c39.69-3.451 75.907-36.938 82.458-76.234l34.366-206.194c3.448-20.677-1.952-41.243-14.813-56.424zm-35.69 48.006l-34.366 206.194c-2.699 16.186-20.043 32.221-36.39 33.645l-514.214 44.714-50.874-305.246h618.314c5.968 0 10.995 2.054 14.155 5.782 3.157 3.73 4.357 9.024 3.376 14.912z"
-              ></path>
-            </svg>
+            <v-badge location="top right" color="red" content="2"
+              ><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  class="path1"
+                  d="M409.6 1024c-56.464 0-102.4-45.936-102.4-102.4s45.936-102.4 102.4-102.4S512 865.136 512 921.6 466.064 1024 409.6 1024zm0-153.6c-28.232 0-51.2 22.968-51.2 51.2s22.968 51.2 51.2 51.2 51.2-22.968 51.2-51.2-22.968-51.2-51.2-51.2z"
+                ></path>
+                <path
+                  class="path2"
+                  d="M768 1024c-56.464 0-102.4-45.936-102.4-102.4S711.536 819.2 768 819.2s102.4 45.936 102.4 102.4S824.464 1024 768 1024zm0-153.6c-28.232 0-51.2 22.968-51.2 51.2s22.968 51.2 51.2 51.2 51.2-22.968 51.2-51.2-22.968-51.2-51.2-51.2z"
+                ></path>
+                <path
+                  class="path3"
+                  d="M898.021 228.688C885.162 213.507 865.763 204.8 844.8 204.8H217.954l-5.085-30.506C206.149 133.979 168.871 102.4 128 102.4H76.8c-14.138 0-25.6 11.462-25.6 25.6s11.462 25.6 25.6 25.6H128c15.722 0 31.781 13.603 34.366 29.112l85.566 513.395C254.65 736.421 291.929 768 332.799 768h512c14.139 0 25.6-11.461 25.6-25.6s-11.461-25.6-25.6-25.6h-512c-15.722 0-31.781-13.603-34.366-29.11l-12.63-75.784 510.206-44.366c39.69-3.451 75.907-36.938 82.458-76.234l34.366-206.194c3.448-20.677-1.952-41.243-14.813-56.424zm-35.69 48.006l-34.366 206.194c-2.699 16.186-20.043 32.221-36.39 33.645l-514.214 44.714-50.874-305.246h618.314c5.968 0 10.995 2.054 14.155 5.782 3.157 3.73 4.357 9.024 3.376 14.912z"
+                ></path></svg
+            ></v-badge>
+
             <h6>Cart</h6>
           </div>
           <div class="icons">
@@ -109,14 +131,26 @@
             </svg>
             <span>Help</span>
           </div>
-          <div class="lang d-flex align-center" id="lang-toggle">
+          <div
+            class="lang d-flex align-center"
+            id="lang-toggle"
+            style="gap: 5px"
+          >
             <span v-html="selected[0].icon"></span>
-            <span>{{ selected[0].lang }}/{{ selected[0].currency }}</span>
+            <span
+              >{{ selected[0].lang }}/{{ selected[0].currency }}
+              <v-icon icon="mdi-arrow-down"></v-icon
+            ></span>
             <v-menu activator="#lang-toggle">
               <v-list v-model:selected="selected">
-                <v-list-item v-for="lang in langs" :key="lang.id">
-                  <span v-html="lang.icon"></span>
-                  {{ lang.lang }} / {{ lang.currency }}
+                <v-list-item v-for="lang in langs" :key="lang.id" :value="lang">
+                  <v-list-item-title
+                    class="d-flex align-center"
+                    style="gap: 10px"
+                  >
+                    <span v-html="lang.icon"></span>
+                    {{ lang.lang }} / {{ lang.currency }}
+                  </v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-menu>
@@ -245,7 +279,14 @@ ul {
     }
   }
 }
+.search {
+  input {
+    border-radius: 16px;
+    outline: none;
+  }
+}
 .lang {
+  cursor: pointer;
   svg {
     width: 20px;
   }
