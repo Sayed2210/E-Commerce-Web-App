821198dc6e8cda9425ac8ef9f312cab01a91aa53
Sayed2210 <eh600039@gmail.com>
2023-10-07 17:44:57 +0300

    nav responsive

--- src/components/global/AppLayout.vue
+++ src/components/global/AppLayout.vue
@@ -1,14 +1,18 @@
 <template>
   <v-layout style="position: relative">
     <CartDrawer />
-    <AppNav v-if="$route.name != 'checkout'" />
+    <AppNav v-show="$route.name != 'checkout' && widowWidth > 990" />
+    <MenuDrawer />
     <v-main
-      :style="`padding-top: ${$route.name == 'checkout' ? '0' : '136px'}`"
+      :style="`padding-top: ${
+        $route.name == 'checkout' ? '0px' : widowWidth <= 990 ? '60px' : '136'
+      }`"
     >
       <slot></slot>
     </v-main>
-    <FixedNav v-if="$route.name != 'checkout'" />
-    <AppFooter v-if="$route.name != 'checkout'" />
+    <ResponsiveNav v-show="$route.name != 'checkout' && widowWidth <= 990" />
+    <FixedNav v-show="$route.name != 'checkout' && widowWidth > 990" />
+    <AppFooter v-show="$route.name != 'checkout'" />
   </v-layout>
 </template>
 
@@ -17,4 +21,24 @@ import AppFooter from "./AppFooter.vue";
 import FixedNav from "./FixedNav.vue";
 import AppNav from "./AppNav.vue";
 import CartDrawer from "./CartDrawer.vue";
+</script>
+
+<script>
+import ResponsiveNav from "@/components/global/ResponsiveNav.vue";
+import MenuDrawer from "@/components/global/MenuDrawer.vue";
+export default {
+  components: {
+    ResponsiveNav,
+    MenuDrawer,
+  },
+  data: () => ({
+    widowWidth: 0,
+  }),
+  mounted() {
+    this.widowWidth = window.innerWidth;
+    window.onresize = () => {
+      this.widowWidth = window.innerWidth;
+    };
+  },
+};
 </script>
